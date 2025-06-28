c0ae3ed64585afeb4650fc9034e4d71cd2c48fff
u2re-dev <c24b@u2re.ru>
2025-06-28 07:27:25 +0700

    Rehaul Input Shifting

--- src/components/input-ext/InputExt.ts
+++ src/components/input-ext/InputExt.ts
@@ -1,11 +1,10 @@
 import { setStyleProperty, bindDraggable } from "u2re/dom";
 import { makeShiftTrigger } from "../../interface/Trigger";
-import { ref, subscribe } from "u2re/object";
+import { bindWith, handleStyleChange } from "u2re/lure";
+import { computed, ref } from "u2re/object";
 
 //
 export const dragSlider = (container, handler, input)=>{ // @ts-ignore
-
-    //
     const correctOffset = (dragging)=>{
 
         // reset dragging offset
@@ -13,27 +12,33 @@ export const dragSlider = (container, handler, input)=>{ // @ts-ignore
         return [0, 0];
     };
 
+    //
+    const computeRelation = ()=>{
+        if (input?.type == "number" || input?.type == "range")
+            { const range = (parseFloat(input?.max || 0) - parseFloat(input?.min || 0)); return ((dragging?.[0]?.value || 0) / (container?.offsetWidth || 1)) * range; } else
+        if (input?.type == "checkbox") { return Math.sign(dragging?.[0]?.value); } else
+        if (input?.type == "radio") {
+            const all = [...input?.parentNode?.querySelectorAll?.('input[type="radio"]')], len = all?.length;
+            return ((dragging?.[0]?.value || 0) / (container?.offsetWidth || 1)) * Math.max(len - 1, 0);
+        }
+        return dragging?.[0]?.value;
+    }
+
     // TODO: support animation
     const resolveDragging = (dragging) => {
         // in case of input is HTMLInputElement
+        const relate = computeRelation();
+
+        //
         if (input?.type == "number" || input?.type == "range") {
-            const vshift = ((dragging?.[0]?.value || 0) / (container?.offsetWidth || 1)) * (parseFloat(input?.max || 0) - parseFloat(input?.min || 0));
-            if (vshift != 0) {
-                input.valueAsNumber += vshift;
-                input.dispatchEvent(new Event("change", { bubbles: true }));
-            }
+            if (relate != 0) { input.valueAsNumber += relate; input?.dispatchEvent?.(new Event("change", { bubbles: true })); }
         } else
         if (input?.type == "checkbox") {
-            if ((dragging?.[0]?.value || 0) != 0) {
-                if (dragging?.[0]?.value > 0 && !input?.checked) { input?.click?.(); }
-                if (dragging?.[0]?.value < 0 &&  input?.checked) { input?.click?.(); }
-            }
+            if (relate > 0 && !input?.checked || relate < 0 &&  input?.checked) { input?.click?.(); }
         } else
         if (input?.type == "radio") {
-            const all = [...input?.parentNode?.querySelectorAll?.('input[type="radio"]')];
-            const len = all?.length, nth = all.indexOf(input);
-            const vshift = Math.floor(((dragging?.[0]?.value || 0) / (container?.offsetWidth || 1)) * Math.max(len - 1, 0));
-            if (vshift != 0 && nth >= 0) { all[Math.max(Math.min(nth + vshift, len - 1), 0)]?.click?.(); };
+            const all = [...input?.parentNode?.querySelectorAll?.('input[type="radio"]')], len = all?.length, nth = all.indexOf(input);
+            if (relate != 0 && nth >= 0) { all[Math.max(Math.min(nth + Math.round(relate), len - 1), 0)]?.click?.(); };
         }
 
         // reset dragging coordinate
@@ -42,7 +47,11 @@ export const dragSlider = (container, handler, input)=>{ // @ts-ignore
     };
 
     //
-    const dragging = [ ref(0), ref(0) ]; subscribe([dragging, "value"], (v)=>setStyleProperty(handler, "--drag-x", v));
+    const dragging = [ ref(0), ref(0) ];
+    bindWith(handler, "--drag-x", dragging?.[0], handleStyleChange);
+    bindWith(handler, "--relate", computed(dragging?.[0], (v)=>computeRelation()), handleStyleChange);
+
+    //
     const customTrigger = (doGrab)=>handler.addEventListener("pointerdown", makeShiftTrigger((ev)=>{correctOffset(dragging); doGrab?.(ev, handler)}));
     const handleValue = (ev)=>{
         const inp = ev?.target ?? input;

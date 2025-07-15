d1c80d440134864683c0d47c06474c478b1de3fc
u2re-dev <c24b@u2re.ru>
2025-07-16 05:01:00 +0700

    WIP Port Color System

---
+++ src/helpers/design/StyleRules.ts
@@ -0,0 +1,22 @@
+import { setStyleRule } from "fest/dom";
+
+//
+export type StyleTuple = [selector: string, sheet: object];
+export const updateThemeBase = async (originColor: string|null = null, $cssIsDark: boolean|null = null)=>{
+    if (originColor != null && localStorage.getItem("--tm-origin") != originColor) localStorage.setItem("--tm-origin", originColor);
+    if ($cssIsDark != null && (!!localStorage.getItem("--tm-scheme") != $cssIsDark)) localStorage.setItem("--tm-scheme", $cssIsDark as unknown as string);
+
+    //
+    setStyleRule(":host, :root, :scope, :where(*)", {
+        "--tm-origin": localStorage.getItem("--tm-origin") || "oklch(20% 0.01 180deg)",
+        "--tm-scheme": (localStorage.getItem("--tm-scheme") ? 1 : 0) || 0
+    });
+}
+
+//
+requestIdleCallback(()=>updateThemeBase(), {timeout: 100});
+addEventListener("storage", (event) => {
+    if (event.key == "--tm-origin" || event.key == "--tm-scheme") {
+        updateThemeBase();
+    }
+});

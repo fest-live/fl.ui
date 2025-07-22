e504ae35e8234712dd8cd344898ba49137c7f1d0
u2re-dev <c24b@u2re.ru>
2025-07-22 17:04:36 +0700

    WIP better test CTM-2

--- test/ctm-2.ts
+++ test/ctm-2.ts
@@ -1,30 +1,35 @@
 import { H, M } from "fest/lure";
 import "../src/scss/index.scss";
 
-//
 const tones = [0, 0.25, 0.5, 0.75, 1];
 
-//
-const gens1 = M(tones, (tone) => H`<div class="c2-themed" style=${{
-    "inlineSize": "100px",
-    "blockSize": "100px",
-    "--surface-tone": `${tone}`,
-    "--detected-theme": "1",
-    "colorScheme": "only light",
-}}></div>`)?.element;
+function makeDecorativeBlocks(className, theme, colorScheme) {
+    return H`
+        <div class="color-row">
+            ${M(tones, (tone) => H`<div classList=${["color-block", className]} style=${{
+                "--contrast-tone": `${tone}`,
+                "--surface-tone": `${tone}`,
+                "--detected-theme": `${theme}`,
+                "colorScheme": colorScheme,
+            }}>
+            <span class="tone-label">${tone}</span>
+            </div>
+        `)?.element}</div>`;
+}
 
-const gens2 = M(tones, (tone) => H`<div class="c2-themed" style=${{
-    "inlineSize": "100px",
-    "blockSize": "100px",
-    "--surface-tone": `${tone}`,
-    "--detected-theme": "0",
-    "colorScheme": "only dark",
-}}></div>`)?.element;
 
 //
-const colorTestL = H`<div style="display: inline-block;">${gens1}</div>`;
-const colorTestD = H`<div style="display: inline-block;">${gens2}</div>`;
+const testTestTone = (name: string)=>H`
+    <div class="color-section">
+        <div class="color-title">Light Version (${name})</div>
+        ${makeDecorativeBlocks(name, 1, "only light")}
+    </div>
+    <div class="color-section">
+        <div class="color-title">Dark Version (${name})</div>
+        ${makeDecorativeBlocks(name, 0, "only dark")}
+    </div>
+`;
 
-//
-document.body.append(colorTestL);
-document.body.append(colorTestD);
+
+document.body.append(testTestTone("c2-surface"));
+document.body.append(testTestTone("c2-contrast"));

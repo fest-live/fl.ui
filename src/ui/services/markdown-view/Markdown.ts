caeea61219e093563e0f59de682142cf20bbebd1
u2re-dev <c24b@u2re.ru>
2025-09-29 06:31:38 +0700

    Markdown better design

--- src/ui/services/markdown-view/Markdown.ts
+++ src/ui/services/markdown-view/Markdown.ts
@@ -20,26 +20,28 @@ export class MarkdownView extends HTMLElement {
 
     //
     async setHTML(doc = "") {
-        let once = false;
         const view = this.#view?.element;
-        const html = H(DOMPurify?.sanitize?.(await doc || "") || view?.innerHTML || "");
         if (view) {
             view.innerHTML = ``;
-            view.append(html);
+            view.append(H(DOMPurify?.sanitize?.(await doc || "") || view?.innerHTML || ""));
         }
-        if (!once) document.dispatchEvent(new CustomEvent("ext-ready", {}));
-        once = true;
+        document.dispatchEvent(new CustomEvent("ext-ready", {}));
     }
 
     //
     async renderMarkdown(file) {
+        if (!file) return;
+
+        //
         typeof file == "string" ? (localStorage.setItem("$cached-md$", file)) : file?.text?.()?.then?.((t) => localStorage.setItem("$cached-md$", t));
-        if (file && navigator?.storage) { provide("/user/cache/last.md", true)?.then?.((p) => p?.write?.(file instanceof Response ? file?.blob?.() : file)); }
+        if (file && navigator?.storage) { provide("/user/cache/last.md", true)?.then?.(async (p) => p?.write?.(file instanceof Response ? await file?.blob?.()?.catch?.(console.warn.bind(console)) : file)); }
+
+        //
         if (typeof file == "string") {
-            this.setHTML(await marked(file));
+            this.setHTML(await marked(file))?.catch?.(console.warn.bind(console));
         } else
             if (file instanceof File || file instanceof Blob || file instanceof Response) {
-                file?.text()?.then?.(async (doc) => this.setHTML(await marked(doc)));
+                file?.text()?.then?.(async (doc) => this.setHTML(await marked(doc))?.catch?.(console.warn.bind(console)))?.catch?.(console.warn.bind(console));
             }
     }
 
@@ -47,7 +49,7 @@ export class MarkdownView extends HTMLElement {
     attributeChangedCallback(name, oldValue) {
         const nv = this.getAttribute("src");
         if (nv && name == "src" && oldValue != nv) {
-            provide(nv || "")?.then?.((file) => this.renderMarkdown(file))?.catch?.(console.error);
+            provide(nv || "")?.then?.((file) => this.renderMarkdown(file))?.catch?.(console.warn.bind(console));
         };
     }
 
@@ -64,7 +66,9 @@ export class MarkdownView extends HTMLElement {
         //
         requestAnimationFrame(() => {
             if (this.getAttribute("src")) {
-                provide(this.getAttribute("src") || "")?.then?.((file) => this.renderMarkdown(file))?.catch?.(console.error);
+                provide(this.getAttribute("src") || "")
+                    ?.then?.((file) => this.renderMarkdown(file))
+                    ?.catch?.(console.warn.bind(console));
             }
         });
     }

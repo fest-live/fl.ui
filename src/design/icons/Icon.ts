810dd029369d224c914604f48c52605a282298e0
u2re-dev <c24b@u2re.ru>
2025-10-02 12:22:19 +0700

    WIP Better Performance

--- src/design/icons/Icon.ts
+++ src/design/icons/Icon.ts
@@ -1,6 +1,6 @@
 import { defineElement, property, E } from "fest/lure";
 import { camelToKebab, preloadStyle } from "fest/dom";
-import { subscribe } from "fest/object";
+import { autoRef, numberRef } from "fest/object";
 import { UIElement } from "../base/UIElement";
 
 // @ts-ignore
@@ -10,6 +10,7 @@ const styled  = preloadStyle(styles);
 //
 //import * as icons from "lucide";
 const iconMap = new Map<string, Promise<string>>();
+const maskCache = new Map<string, string>();
 
 //
 const isPathURL = (url: string)=>{ return URL.canParse(url, location.origin) || URL.canParse(url, "localhost"); }
@@ -39,28 +40,39 @@ export class UIPhosphorIcon extends UIElement {
     @property({ source: "attr" }) icon: any = "";
     @property({ source: "attr" }) iconStyle: any = "duotone";
     @property({ source: "attr" }) width?: number;
-    @property() protected iconElement?: SVGElement;
+    @property({ source: "attr" }) size?: string;
+    #maskRef = autoRef("");
     #options = { padding: 0, icon: "", iconStyle: "duotone" };
 
     // also "display" may be "contents"
     public styles = ()  => styled.cloneNode(true);
-    public onRender() { this.icon = this.#options?.icon || this.icon; this.iconStyle = this.#options?.iconStyle || this.iconStyle; this.updateIcon(); subscribe([(this as any).getProperty("icon"), "value"], (icon)=>{ this.updateIcon(icon) }); }
+    public onRender() {
+        this.icon = this.#options?.icon || this.icon;
+        this.iconStyle = this.#options?.iconStyle || this.iconStyle;
+        this.updateIcon();
+    }
     constructor(options = {icon: "", padding: ""}) { super(); Object.assign(this.#options, options); }
 
     //
     protected updateIcon(icon?: any) {
-        if (icon ||= (this.icon?.value ?? (typeof this.icon == "string" ? this.icon : "")) || "") { // @ts-ignore
-            const ICON = camelToKebab(icon || "");//capitalizeFirstLetter(kebabToCamel(icon || ""));
-            const self = this as any;
-            loadAsImage(`./assets/icons/${this?.iconStyle || "duotone"}/${ICON}-${this?.iconStyle || "duotone"}.svg`)?.then?.((url)=>{
-                if (!url) return;
-                const src  = `url(\"${url}\")`;
-                const fill = self;//self?.shadowRoot?.querySelector?.(".fill");
-                if (fill?.style?.getPropertyValue?.("mask-image") != src) {
-                    fill?.style?.setProperty?.("mask-image", src);
-                }
-            });
-        }
+        const nextIcon = icon ?? (this.icon?.value ?? (typeof this.icon === "string" ? this.icon : ""));
+        if (!nextIcon) { return this; }
+
+        const ICON = camelToKebab(nextIcon || "");
+        const urlPromise = loadAsImage(`./assets/icons/${this?.iconStyle || "duotone"}/${ICON}-${this?.iconStyle || "duotone"}.svg`);
+
+        urlPromise?.then?.((url) => {
+            if (!url) return;
+            let maskValue = maskCache.get(url);
+            if (!maskValue) {
+                maskValue = `url(\"${url}\")`;
+                maskCache.set(url, maskValue);
+            }
+            if (this.#maskRef.value !== maskValue) {
+                this.#maskRef.value = maskValue;
+                this.style.setProperty("--ui-icon-mask", maskValue);
+            }
+        });
         return this;
     }
 
@@ -69,7 +81,9 @@ export class UIPhosphorIcon extends UIElement {
     public onInitialize() {
         super.onInitialize?.(); const self = this as unknown as HTMLElement;
         E(self, { classList: new Set(["ui-icon", "u2-icon"]), inert: true });
-        if (!self?.style.getPropertyValue("padding") && this.#options?.padding) { self.style.setProperty("padding", typeof this.#options?.padding == "number" ? (this.#options?.padding + "rem") : this.#options?.padding); };
+        if (!self?.style.getPropertyValue("padding") && this.#options?.padding) { self.style.setProperty("--icon-padding", typeof this.#options?.padding == "number" ? (this.#options?.padding + "rem") : this.#options?.padding); };
+        if (this.size) { self.style.setProperty("--icon-size", this.size); }
+        this.style.setProperty("--ui-icon-mask", this.#maskRef.value || "none");
         this.updateIcon();
     }
 }

c95e24216715d86c61617e128d3ccd3dd0104977
u2re-dev <c24b@u2re.ru>
2025-10-20 22:11:29 +0700

    Markdown rendering fix

--- src/ui/services/markdown-view/Markdown.ts
+++ src/ui/services/markdown-view/Markdown.ts
@@ -3,45 +3,73 @@ import styles from "./Markdown.scss?inline&compress";
 import DOMPurify from 'isomorphic-dompurify';
 import { marked } from "marked";
 import { provide, E, H } from "fest/lure";
+
+//
 import markedKatex from "marked-katex-extension";
 marked?.use?.(markedKatex({ throwOnError: false, nonStandard: true }));
 
 //
 const preInit = URL.createObjectURL(new Blob([styles], { type: "text/css" }));
 export class MarkdownView extends HTMLElement {
-    static observedAttributes = ["src"];
-
-    //
+    static observedAttributes = ["src"]; #view;
     constructor() { super(); this.createShadowRoot(); }
 
     //
-    #view;
-    #themeStyle;
-
-    //
     async setHTML(doc = "") {
-        const view = this.#view?.element;
+        const view = this.#view;
         if (view) {
-            view.innerHTML = ``;
-            view.append(H(DOMPurify?.sanitize?.(await doc || "") || view?.innerHTML || ""));
+            view.innerHTML = ``; console.log(doc);
+            view.append(H(DOMPurify?.sanitize?.((await doc)?.trim?.() || "") || view?.innerHTML || ""));
         }
         document.dispatchEvent(new CustomEvent("ext-ready", {}));
     }
 
     //
+    async loadFromCache() {
+        if (navigator?.storage) {
+            return provide("/user/cache/last.md");
+        }
+        return localStorage.getItem("$cached-md$");
+    }
+
+    //
+    async writeToCache(text: string|File|Blob) {
+        if (navigator?.storage) {
+            const forWrite = await provide("/user/cache/last.md", true);
+            await forWrite?.write?.(text instanceof Response ? await text?.blob?.()?.catch?.(console.warn.bind(console)) : text)
+            await forWrite?.close?.();
+        } else
+        if (typeof text == "string") {
+            localStorage.setItem("$cached-md$", text)
+        }
+    }
+
+    //
     async renderMarkdown(file) {
-        if (!file) return;
+        //
+        const renderMarkdownText = async (text: string)=>{
+            this.writeToCache(text)?.catch?.(console.warn.bind(console));
+            return this.setHTML(await marked(text?.trim?.() || ""))?.catch?.(console.warn.bind(console));
+        }
 
         //
-        typeof file == "string" ? (localStorage.setItem("$cached-md$", file)) : file?.text?.()?.then?.((t) => localStorage.setItem("$cached-md$", t));
-        if (file && navigator?.storage) { provide("/user/cache/last.md", true)?.then?.(async (p) => p?.write?.(file instanceof Response ? await file?.blob?.()?.catch?.(console.warn.bind(console)) : file)); }
+        if (!file) {
+            const cached = await this.loadFromCache();
+            if (cached) { return this.renderMarkdown(cached || "")?.catch?.(console.warn.bind(console)); };
+            return;
+        };
+
+        //
+        if (URL.canParse(file) || file?.startsWith?.("blob:")) {
+            file = await (await fetch(file))?.text?.();
+        }
 
         //
         if (typeof file == "string") {
-            this.setHTML(await marked(file))?.catch?.(console.warn.bind(console));
+            renderMarkdownText(file)?.catch?.(console.warn.bind(console));
         } else
             if (file instanceof File || file instanceof Blob || file instanceof Response) {
-                file?.text()?.then?.(async (doc) => this.setHTML(await marked(doc))?.catch?.(console.warn.bind(console)))?.catch?.(console.warn.bind(console));
+                file?.text()?.then?.(async (doc) => renderMarkdownText(doc)?.catch?.(console.warn.bind(console)))?.catch?.(console.warn.bind(console));
             }
     }
 
@@ -49,28 +77,17 @@ export class MarkdownView extends HTMLElement {
     attributeChangedCallback(name, oldValue) {
         const nv = this.getAttribute("src");
         if (nv && name == "src" && oldValue != nv) {
-            provide(nv || "")?.then?.((file) => this.renderMarkdown(file))?.catch?.(console.warn.bind(console));
+            this.renderMarkdown(nv || "")?.catch?.(console.warn.bind(console));
         };
     }
 
     //
     createShadowRoot() {
         const shadowRoot = this.attachShadow({ mode: "open" });
-        shadowRoot.append((this.#view = E("div.markdown-body", { dataset: { print: "" } }))?.element);
-
-        //
         const style = document.createElement("style");
         style.innerHTML = `@import url("${preInit}");`;
-        shadowRoot.appendChild(style);
-
-        //
-        requestAnimationFrame(() => {
-            if (this.getAttribute("src")) {
-                provide(this.getAttribute("src") || "")
-                    ?.then?.((file) => this.renderMarkdown(file))
-                    ?.catch?.(console.warn.bind(console));
-            }
-        });
+        shadowRoot.append(style, this.#view = E("div.markdown-body", { dataset: { print: "" } }));
+        requestAnimationFrame(() => this.renderMarkdown(this.getAttribute("src") || ""));
     }
 }
 

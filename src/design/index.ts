b08129a41ecc5cdcec554e1d15025fa0396f9890
u2re-dev <c24b@u2re.ru>
2025-10-23 23:16:34 +0700

    Design utils in LUR.E

--- src/design/index.ts
+++ src/design/index.ts
@@ -1,6 +1,3 @@
 export * from "./base/UIElement";
-export * from "./engine/ThemeEngine";
-export * from "./engine/DynamicEngine";
-export * from "./engine/ImagePicker";
 export * from "./appearance/Desktop";
 export * from "./appearance/Mobile";

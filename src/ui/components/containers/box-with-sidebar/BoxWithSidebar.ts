4b07d71c0aaf2a5bda38059562515f1abdca324b
u2re-dev <c24b@u2re.ru>
2025-10-24 06:07:21 +0700

    Allow customize

--- src/ui/components/containers/box-with-sidebar/BoxWithSidebar.ts
+++ src/ui/components/containers/box-with-sidebar/BoxWithSidebar.ts
@@ -56,7 +56,7 @@ export class BoxWithSidebar extends UIElement {
     //
     styles = () => styled?.cloneNode?.(true);
     render = function () {
-        return H`<div class="bar c2-surface"><button part="open-sidebar" class="open-sidebar c2-surface" on:click=${() => { this.sidebarOpened.value = !this.sidebarOpened.value; }}><ui-icon icon="${conditional(this.sidebarOpened, 'text-outdent', 'list')}"></ui-icon></button><slot name="bar"></slot></div>
+        return H`<div part="bar" class="bar c2-surface"><button part="open-sidebar" class="open-sidebar c2-surface" on:click=${() => { this.sidebarOpened.value = !this.sidebarOpened.value; }}><ui-icon icon="${conditional(this.sidebarOpened, 'text-outdent', 'list')}"></ui-icon></button><slot name="bar"></slot></div>
     <div part="content-box" class="content-box"><div part="sidebar" class="sidebar" data-visible=${this.sidebarOpened}><slot name="sidebar"></slot></div><div part="content" class="content"><slot></slot></div></div>`;
     }
 }

c905a37bc907e7d906661732371be1949905d65b
u2re-dev <c24b@u2re.ru>
2025-12-26 16:42:51 +0700

    No strict mathml

--- src/services/markdown-view/Markdown.ts
+++ src/services/markdown-view/Markdown.ts
@@ -7,7 +7,7 @@ import { provide, E, H } from "fest/lure";
 //
 import markedKatex from "marked-katex-extension";
 import { preloadStyle } from "fest/dom";
-marked?.use?.(markedKatex({ throwOnError: false, nonStandard: true, output: "mathml" }));
+marked?.use?.(markedKatex({ throwOnError: false, nonStandard: true, output: "mathml", strict: false }));
 
 //
 const styled = preloadStyle(styles);

bad75e1ed6aaff931eecb72463a41a4d899ef50b
L2NE-dev <conway24b@gmail.com>
2026-04-04 21:27:55 +0700

    Needs new styling and library system....

--- src/index.ts
+++ src/index.ts
@@ -70,7 +70,7 @@ export * from "./ui/index";
 
 //
 export * from "./services/file-manager";
-export * from "./services/markdown-view/ts/Markdown";
+export * from "./ui/markdown-view/ts/Markdown";
 
 //
 (async () => {
--- src/services/file-manager/index.ts
+++
@@ -1,5 +0,0 @@
-export { FileManager, default } from "./ts/FileManager";
-export { FileManagerContent } from "./ts/FileManagerContent";
-export { FileOperative, type FileEntryItem, type EntryKind } from "./ts/Operative";
-export { createItemCtxMenu } from "./ts/ContextMenu";
-export type * from "./types";

c4d76d99b5b20cb26de6b2504efedf39972e140e
L2NE-dev <conway24b@gmail.com>
2026-05-04 16:50:53 +0700

    Moving to dedicated libs

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -8,17 +8,6 @@ export * from "./navigation/appearance/Desktop";
 export * from "./navigation/appearance/Mobile";
 
 //
-export * from "./misc/Toast";
-export * from "./misc/Format";
-export * from "./misc/Canvas-2";
-
-//
-export * from "./markdown/Markdown";
-export * from "./markdown/Toolbar";
-export * from "./markdown/Viewer";
-
-//
-export * from "./explorer/FileManager";
-export * from "./explorer/Operative";
-export * from "./explorer/ContextMenu";
-export * from "./explorer/utils";
+export * from "../misc/Toast";
+export * from "../misc/Format";
+export * from "../misc/Canvas-2";
